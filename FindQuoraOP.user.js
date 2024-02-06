3368f75838bff0af6cbd4889239f99535a55865a
Christopher Burke <chris@cyberabi.com>
2024-02-05 22:44:13 -0800

    Create FindQuoraOP.user.js
    Initial commit.

---
+++ FindQuoraOP.user.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         Quora Question OP Finder
+// @namespace    http://cyberabi.com/
+// @version      0.1
+// @description  Find the OP of a Quora question from the log page
+// @author       Christopher Burke
+// @match        https://www.quora.com/*/log
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=quora.com
+// @updateURL
+// @downloadURL
+// @require http://code.jquery.com/jquery-latest.js
+// ==/UserScript==
+
+$(document).ready(function() {
+    'use strict';
+
+    const opMarker1 = 'Question';
+    const opRegEx1 = /Question\s*added\s*by\s*/;
+    //const opRegEx2 = 'https://www.quora.com/profile/.*';
+
+    // Scroll until we reach the bottom
+    var _autoScroller = setInterval(function() {
+        // See if what we want is on the page
+        var elementsA = document.querySelectorAll('a');
+        var found = false;
+        elementsA.forEach((child) => {
+            if (!found && child.innerText === opMarker1) {
+                const parentText = child.parentNode.innerText;
+                if (parentText.search(opRegEx1) === 0) {
+                    // Found the OP's name and profile link
+                    // Make sure it's visible
+                    clearInterval(_autoScroller);
+                    found = true;
+                    window.scrollTo(0, window.scrollY + window.innerHeight);
+                    alert(parentText);
+                }
+            }
+        });
+        // Spam scrolling until we reach the bottom
+        if (!found) {
+            // Load more
+            window.scrollTo(0, window.scrollY + window.innerHeight);
+        }
+    }, 100);
+})();
